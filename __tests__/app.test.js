282da6620af37121b941aadfe3df6f9057621825
NikitaSelanko <nikitaselanko@gmail.com>
2022-10-10 14:42:47 +0100

    Made more deep tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,16 @@
 const request = require("supertest");
 const app = require("../app");
 const db = require("../db/connection");
-const seed = require("../db/seeds/seed")
-const {categoryData, commentData, reviewData, userData} = require("../db/data/test-data")
+const seed = require("../db/seeds/seed");
+const {
+  categoryData,
+  commentData,
+  reviewData,
+  userData,
+} = require("../db/data/test-data");
 
 beforeEach(() => {
-  return seed({categoryData, commentData, reviewData, userData});
+  return seed({ categoryData, commentData, reviewData, userData });
 });
 afterAll(() => {
   return db.end();
@@ -14,12 +19,36 @@ afterAll(() => {
 describe("Categories tests", () => {
   describe("Get/categories", () => {
     test("status 200, should responde withan array of categories ", () => {
-        return request(app)
+      return request(app)
         .get("/api/categories")
         .expect(200)
-        .then((result)=>{
-            expect(result.body.categories).toBeInstanceOf(Array)
-        })
+        .then((result) => {
+          const catArr = result.body.categories;
+          expect(catArr).toBeInstanceOf(Array);
+          expect(catArr.length).toBe(4)
+          catArr.forEach((category)=>{
+            expect(category.hasOwnProperty("slug")).toBe(true)
+            expect(category.hasOwnProperty("description")).toBe(true)
+
+          })
+        });
+    });
+    test("status 200, should responde withan array of category objects, each of which should have the following properties: slug & description properties", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then((result) => {
+          const catArr = result.body.categories;
+          console.log(catArr, "TEST <<<<<<<<<<<<<<<");
+          catArr.forEach((category) => {
+            expect(catArr[0]).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+        });
     });
   });
 });
--- models/categories-model.js
+++ models/categories-model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 
 exports.selectCategories = () => {
     return db.query("SELECT * FROM categories;").then((data)=>{
-        console.log(data.rows)
+        
         return data.rows;
     })
 }
