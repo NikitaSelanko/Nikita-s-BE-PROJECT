896e5aabd3c962e9ec7946573c447c1c84b9bf77
NikitaSelanko <nikitaselanko@gmail.com>
2022-10-11 14:07:59 +0100

    done proper commit

---
+++ controllers/error-controllers.js
@@ -0,0 +1,15 @@
+exports.handleIternalErrors = (err, req, res, next) => {
+    res.status(500).send({ msg: "internal server error" })
+}
+exports.handlePSQLErrors = (err, req, res, next) => {
+    if (err.code === "22P02") {
+        res.status(400).send({ msg: "wrong input" });
+      } else {
+        next(err)
+      }
+}
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+      res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+  };
--- controllers/reviews-controller.js
+++ controllers/reviews-controller.js
@@ -1,13 +1,25 @@
-const {selectReviewsByID} = require("../models/reviews-model")
+const { selectReviewsByID, updateVotes } = require("../models/reviews-model");
 
+exports.getReviewByID = (req, res, next) => {
+  const id = req.params.review_id;
+  selectReviewsByID(id)
+    .then((review) => {
+      res.status(200).send({ review });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
-exports.getReviewByID = ((req, res, next)=>{
-    const id = req.params.review_id
-    selectReviewsByID(id).then((review)=>{
-        res.status(200).send({review})
+exports.patchVotes = (req, res, next) => {
+  const review_id = req.params.review_id;
+  const votes = req.body.inc_votes;
+
+  updateVotes(review_id, votes)
+    .then((review) => {
+      res.status(200).send({ review });
     })
-    .catch((err)=>{
-        next(err)
-    })
-    
-})
+    .catch((err) => {
+      next(err);
+    });
+};
