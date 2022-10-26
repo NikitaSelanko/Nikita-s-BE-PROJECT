933ab2034db10d20ecbf8f020e110a96f785ee58
NikitaSelanko <nikitaselanko@gmail.com>
2022-10-26 14:56:31 +0100

    done with test

--- models/comments-model.js
+++ models/comments-model.js
@@ -1,5 +1,6 @@
 const db = require("../db/connection");
 
+
 exports.fetchCommentsByreviewID = (review_id) => {
   return db
     .query(
@@ -13,14 +14,33 @@ exports.fetchCommentsByreviewID = (review_id) => {
     )
     .then(({ rows }) => {
       if (rows.length === 0) {
-        return db.query(
-          `SELECT * FROM reviews WHERE review_id = $1;`,[review_id])
-          .then(({rowCount})=>{
-          console.log(rowCount)
-          if(rowCount){
-            return []
-          }return Promise.reject({ status: 404, msg: "ID NOT FOUND" });
-        }) 
-      } return rows
+        return db
+          .query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
+          .then(({ rowCount }) => {
+            console.log(rowCount);
+            if (rowCount) {
+              return [];
+            }
+            return Promise.reject({ status: 404, msg: "ID NOT FOUND" });
+          });
+      }
+      return rows;
     });
 };
+exports.sendCommentByReviewID = (review_id, username, body) => {
+  if(!username || !body){
+    return Promise.reject({status: 400, msg: "wrong input"})
+  }
+  return db.query(
+    `INSERT INTO comments (author, body, review_id)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+    `,[username, body, review_id])
+    .then (({rows}) =>{
+      if(rows.length === 0){
+        return Promise.reject({status:404, msg:"ID NOT FOUND"})
+      }
+      return rows[0];
+    })
+  }
+
